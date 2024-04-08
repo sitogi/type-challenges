55168746ef79558bb5c378a2d262c5399dbe42b8
sitogi <takano162@gmail.com>
2024-04-08 22:46:51 +0900

    solve a nomarl one

--- problems/normal.ts
+++ problems/normal.ts
@@ -152,3 +152,55 @@ import type { Alike, Equal, Expect } from '@type-challenges/utils';
     Expect<Equal<TupleToUnion<[123]>, 123>>,
   ];
 }
+
+/* [Chainable Options](https://github.com/type-challenges/type-challenges/blob/main/questions/00012-medium-chainable-options/README.ja.md)  */
+
+  /* _____________ ここにコードを記入 _____________ */
+
+  type Chainable = {
+    option(key: string, value: any): any
+    get(): any
+  }
+
+
+  declare const a: Chainable
+
+  const result1 = a
+    .option('foo', 123)
+    .option('bar', { value: 'Hello World' })
+    .option('name', 'type-challenges')
+    .get()
+
+  const result2 = a
+    .option('name', 'another name')
+    // @ts-expect-error
+    .option('name', 'last name')
+    .get()
+
+  const result3 = a
+    .option('name', 'another name')
+    // @ts-expect-error
+    .option('name', 123)
+    .get()
+
+  type cases = [
+    Expect<Alike<typeof result1, Expected1>>,
+    Expect<Alike<typeof result2, Expected2>>,
+    Expect<Alike<typeof result3, Expected3>>,
+  ]
+
+  type Expected1 = {
+    foo: number
+    bar: {
+      value: string
+    }
+    name: string
+  }
+
+  type Expected2 = {
+    name: string
+  }
+
+  type Expected3 = {
+    name: number
+  }
